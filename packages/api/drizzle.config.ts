730c6d3beb3e534ed320a4140983ac55b71048fd
Evan Fleet <evfleet@gmail.com>
2025-01-23 22:57:12 -0500

    refactor(api): rename migrations folder (#2)

--- packages/api/drizzle.config.ts
+++ packages/api/drizzle.config.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  out: "./db",
+  out: "./migrations",
   schema: "./src/database/schema.ts",
   dialect: "sqlite",
   dbCredentials: {
